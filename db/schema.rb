eb5540d4f492dd2c9815b8ffe69599a2d51ceb36
emilesu <emilesu@qq.com>
2017-08-01 20:45:01 +0800

    implement admin album views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170801085058) do
+ActiveRecord::Schema.define(version: 20170801123332) do
 
   create_table "albums", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20170801085058) do
     t.string "album_cover"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "articles", force: :cascade do |t|
